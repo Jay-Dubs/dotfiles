0fa26e1bf3036dd72b25c3abd3a0392d9f845d74
Jeremy Wright <jeremywright@goinstant.com>
2012-10-05 18:51:13 -0300

    removed .bashrc added .gitignore

---
+++ generate-sym-links.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+################################################################################
+# File: generate-sym-links.sh
+# Purpose: This script will replace the dot files in your home directory (~)
+# with the dot files in this repo.
+# 
+# The current files are
+# .bashrc:      Bash configuration file
+# .vimrc:       Vim configuration file
+# .vim/:        Vim addon/plugin directory
+# .gitconfig:   git configuration
+#
+################################################################################
+
+
